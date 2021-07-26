558635d73e5a279e458da2a664fd7fe34f944148
nuke-droid <33710624+nuke-droid@users.noreply.github.com>
2021-07-26 14:59:03 -0700

    Update crackhash.py

--- crackhash.py
+++ crackhash.py
@@ -1,8 +1,14 @@
 
 from hash import hash
+import time
+from os import strerror, system
+
+starttime = time.time()
 
 e = (IndexError)
 h = hash()
+
+
 str = input("Enter string to crack: ")
 
 with open('pwd.txt') as f:
@@ -22,13 +28,28 @@ for i in content:
 
         painbow.append([k, l])
 
+ticker = 0
 
 for i in range(len(painbow)):
     try:
         sub = painbow[i]
+        #ystem('clear')
+
+        
 
         if sub[1] == str:
             print(f"Cracked! Password: {sub[0]} Hash: {sub[1]}")
+            ticker += 1
+            endtime = time.time()
+            print("--- %s seconds ---" % (time.time() - starttime))
+            exit()
 
     except e:
         pass
+
+if ticker <= 0:
+    print("No corresponding hash found in database.")
+
+endtime = time.time()
+
+print(endtime - starttime)
