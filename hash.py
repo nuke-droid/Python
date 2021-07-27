90f46f36b72964dc1c5f78ea4bb5ff67c8ace5ce
nuke-droid <33710624+nuke-droid@users.noreply.github.com>
2021-07-27 15:44:31 -0700

    Update hash.py
    Added comments

--- hash.py
+++ hash.py
@@ -1,9 +1,10 @@
 import hashlib
 
 
-
+#custom hash class
 class hash:
-
+    
+    #function which takes in plaintext value as strin str
     def hashgen(self, str):
 
         result = hashlib.sha256(str.encode())
@@ -11,18 +12,19 @@ class hash:
         hashvalue = result.hexdigest()
 
         return hashvalue
-
+    
+    #private function for displaying plaintext hash when using class in script mode
     def _printhash(self):
         str = input("Enter string to encode: ")
 
         print(self.hashgen(str))
 
-
+    #main function for script mode
     def main(self):
         self._printhash()
         
     
-
+#if statement for detecting if main is called. This dicates script behavior or module behavior
 if __name__ == "__main__":
     h = hash()
 
